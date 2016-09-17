43fa8795ef61d19858f68355785ad4ecb306e7df
VijiVarsh <vijibbalu@gmail.com>
2016-09-17 00:00:42 -0400

    Fix for not showing donoritem/wishlist item which are expired

--- givenkind/src/main/java/org/givenkind/repository/DonorlistItemRepository.java
+++ givenkind/src/main/java/org/givenkind/repository/DonorlistItemRepository.java
@@ -1,9 +1,12 @@
 package org.givenkind.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.givenkind.model.DonorlistItem;
+import org.givenkind.model.NonProfitUserLogon;
 import org.givenkind.model.UserLogon;
+import org.givenkind.model.WishlistItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +19,8 @@ public interface DonorlistItemRepository extends JpaRepository<DonorlistItem, Lo
 	List<DonorlistItem> findByUser(UserLogon user);
 	
 	List<DonorlistItem> findAll();
+	
+	@Query("select di from DonorlistItem di " +
+	           "where di.dateExpires>=? and di.user=?")
+		List<DonorlistItem> findByDateExpiresAndUser(Date dateExpires, UserLogon user);
 }
