f6905368a63be63154e5e85dc780d9821d48bf75
VijiVarsh <vijibbalu@gmail.com>
2017-01-22 09:43:24 -0500

    update

--- givenkind/src/main/java/org/givenkind/repository/ProfileRepository.java
+++ givenkind/src/main/java/org/givenkind/repository/ProfileRepository.java
@@ -8,9 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProfileRepository extends JpaRepository<Profile, Long>{
 	
 	public Profile findById(Long Id);
-	//public Profile findByEIN(Long EIN);
-	
-  //public Profile findByProfile(String contactEmail);
 	
 	public List<Profile> findAll();
 
