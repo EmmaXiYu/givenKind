547d9562f881a1ea6fa4f84bd257967e4832c807
Michael F <juliusespinosa@192.168.1.4>
2017-01-22 18:32:37 -0800

    latest war file

--- givenkind/src/main/java/org/givenkind/repository/ProfileRepository.java
+++ givenkind/src/main/java/org/givenkind/repository/ProfileRepository.java
@@ -8,12 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProfileRepository extends JpaRepository<Profile, Long>{
 	
 	public Profile findById(Long Id);
-	//public Profile findByEIN(Long EIN);
-	
-//	public Profile findByProfile(String contactEmail);
 	
 	public List<Profile> findAll();
 
 	public Profile findByEin(String ein);
 	
-}
+	public Profile findByContactEmail(String email);	
+	}
